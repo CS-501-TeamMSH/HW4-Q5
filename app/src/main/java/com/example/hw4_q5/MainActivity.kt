948a150182a031a270e1ddc9886d03956e3a12f9
md499 <mdhir@bu.edu>
2023-10-15 16:36:39 -0400

    play again feature not working

--- app/src/main/java/com/example/hw4_q5/MainActivity.kt
+++ app/src/main/java/com/example/hw4_q5/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.hw4_q5
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
-import com.example.hw4_q5.HangmanGame
-import com.example.hw4_q5.Hangman
-import com.example.hw4_q5.KeyboardFragment
-import com.example.hw4_q5.Word
-
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -47,6 +43,10 @@ class MainActivity : AppCompatActivity() {
 
         word = hangmanGame.selectRandomWord()
         Log.d("word,", word)
+
+        val playAgainButton = findViewById<Button>(R.id.playAgainButton)
+        playAgainButton.visibility = View.GONE
+
         // Landscape
         // val isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
         // val layoutId = if (isLandscape) R.layout.activity_main_landscape else R.layout.activity_main_portrait
@@ -56,15 +56,41 @@ class MainActivity : AppCompatActivity() {
 
     fun play(char: Char) {
         if (hangmanGame.makeGuess(char)) {
-            var index = word.indexOf(char)
-            wordFragment.updateDisplayedWord(char, index)
-        } else {
-            var num = hangmanGame.getRemainingGuesses()
-            hangmanFragment.setHangman(num)
-            if (num == 0) {
-                Toast.makeText(this, "Game Over", Toast.LENGTH_SHORT).show()
+            var indices = mutableListOf<Int>()
+            word.forEachIndexed { index, c ->
+                if (c == char) {
+                    indices.add(index)
+                }
             }
 
+            for (index in indices) {
+                wordFragment.updateDisplayedWord(char, word)
+            }
+
+            if (!wordFragment.wordView.text.toString().contains('_')) {
+                Toast.makeText(this, "You won!", Toast.LENGTH_SHORT).show()
+                val playAgainButton = findViewById<Button>(R.id.playAgainButton)
+                playAgainButton.visibility = View.VISIBLE
+            } else {
+                var num = hangmanGame.getRemainingGuesses()
+                hangmanFragment.setHangman(num)
+                if (num == 0) {
+                    Toast.makeText(this, "Game Over", Toast.LENGTH_SHORT).show()
+                    val playAgainButton = findViewById<Button>(R.id.playAgainButton)
+                    playAgainButton.visibility = View.VISIBLE
+                }
+            }
         }
     }
-}
+
+    fun playAgain(view: View) {
+        // Reset the game or perform any necessary actions to start a new game.
+        // For example, you can reset the word, hangman, and keyboardFragment.
+        // Call the relevant reset functions or recreate the fragments and game state.
+
+        // After resetting, you might want to hide the "Play Again" button again.
+        val playAgainButton = findViewById<Button>(R.id.playAgainButton)
+        playAgainButton.visibility = View.GONE
+    }
+
+    }
